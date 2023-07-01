d7128ca650589ff228a46131e84e9b091165da96
kelshardy <k.hardy44@outlook.com>
2023-07-01 12:14:37 +1000

    Flask Validation for Card Schema

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from init import db, ma, bcrypt, jwt
 from controllers.cli_controllers import db_commands
 from controllers.auth_controller import auth_bp
 from controllers.card_controller import cards_bp
+from marshmallow.exceptions import ValidationError
 
 def create_app():
     app = Flask(__name__)
@@ -15,6 +16,10 @@ def create_app():
     #                                       dbms        driver   user       password url    port    db_name
     app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("DATABASE_URL")
     app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
+    
+    @app.errorhandler(ValidationError)
+    def validation_error(err):
+        return {'error': err.messages}, 400
     
     db.init_app(app)
     ma.init_app(app)
